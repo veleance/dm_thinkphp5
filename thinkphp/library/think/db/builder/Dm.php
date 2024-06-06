f3b8734451151215a0e54743a37d4d75f7a405dd
风浅 <917423509@qq.com>
2024-06-06 14:11:07 +0800

    首次提交

---
+++ thinkphp/library/think/db/builder/Dm.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace think\db\builder;
+
+use think\db\Builder;
+use think\db\Query;
+
+/**
+ * Dm数据库驱动
+ */
+class Dm extends Builder
+{
+    protected $selectSql = 'SELECT * FROM (SELECT thinkphp.*, rownum AS numrow FROM (SELECT  %DISTINCT% %FIELD% FROM %TABLE%%JOIN%%WHERE%%GROUP%%HAVING%%ORDER%) thinkphp ) %LIMIT%%COMMENT%';
+
+    /**
+     * limit分析
+     * @access protected
+     * @param  Query $query 查询对象
+     * @param  mixed $limit
+     * @return string
+     */
+    protected function parseLimit($limit)
+    {
+        $limitStr = '';
+
+        if (!empty($limit)) {
+            $limit = explode(',', $limit);
+
+            if (count($limit) > 1) {
+                $limitStr = "(numrow>" . $limit[0] . ") AND (numrow<=" . ($limit[0] + $limit[1]) . ")";
+            } else {
+                $limitStr = "(numrow>0 AND numrow<=" . $limit[0] . ")";
+            }
+        }
+
+        return $limitStr ? ' WHERE ' . $limitStr : '';
+    }
+
+
+    /**
+     * 设置锁机制
+     * @access protected
+     * @param  Query      $query 查询对象
+     * @param  bool|false $lock
+     * @return string
+     */
+    protected function parseLock($lock = false)
+    {
+        if (!$lock) {
+            return '';
+        }
+
+        return ' FOR UPDATE NOWAIT ';
+    }
+
+
+    /**
+     * 字段和表名处理
+     * @access public
+     * @param  Query  $query  查询对象
+     * @param  string $key
+     * @param  string $strict
+     * @return string
+     */
+    /*public function parseKey($key, $options = [], $strict = false)
+    {
+        print_r($key);
+        if (strpos($key, '->') && false === strpos($key, '(')) {
+            // JSON字段支持
+            list($field, $name) = explode($key, '->');
+            $key                = '"' . $field . '"' . '."' . $name . '"';
+        }
+        return $key;
+    }*/
+    /**
+     * 字段和表名处理
+     * @access protected
+     * @param mixed  $key
+     * @param array  $options
+     * @return string
+     */
+    protected function parseKey($key, $options = [], $strict = false)
+    {
+        if (is_numeric($key)) {
+            return $key;
+        } elseif ($key instanceof Expression) {
+            return $key->getValue();
+        }
+
+        $key = trim($key);
+        if (strpos($key, '$.') && false === strpos($key, '(')) {
+            // JSON字段支持
+            list($field, $name) = explode('$.', $key);
+            return 'json_extract(' . $field . ', \'$.' . $name . '\')';
+        } elseif (strpos($key, '.') && !preg_match('/[,\'\"\(\)`\s]/', $key)) {
+            list($table, $key) = explode('.', $key, 2);
+            if ('__TABLE__' == $table) {
+                $table = $this->query->getTable();
+            }
+            if (isset($options['alias'][$table])) {
+                $table = $options['alias'][$table];
+            }
+        }
+
+        if ($strict && !preg_match('/^[\w\.\*]+$/', $key)) {
+            throw new Exception('not support data:' . $key);
+        }
+        if ('*' != $key && ($strict || !preg_match('/[,\'\"\*\(\)`.\s]/', $key))) {
+            $key = '`' . $key . '`';
+        }
+        if (isset($table)) {
+            if (strpos($table, '.')) {
+                $table = str_replace('.', '`.`', $table);
+            }
+            $key = '`' . $table . '`.' . $key;
+        }
+        return $key;
+    }
+
+
+    /**
+     * table分析
+     * @access protected
+     * @param mixed $tables
+     * @param array $options
+     * @return string
+     */
+    protected function parseTable($tables, $options = [])
+    {
+        $item = [];
+        $database = $this->connection->getConfig('database');
+        foreach ((array) $tables as $key => $table) {
+            if (!is_numeric($key)) {
+                $key    = $this->parseSqlTable($key);
+                $aliasTable  = $this->parseKey($key) . ' ' . (isset($options['alias'][$table]) ? $this->parseKey($options['alias'][$table]) : $this->parseKey($table));
+                $item[] =  $database.'.'.$aliasTable;
+            } else {
+                $table = $this->parseSqlTable($table);
+                if (isset($options['alias'][$table])) {
+                    $aliasTable = $this->parseKey($table) . ' ' . $this->parseKey($options['alias'][$table]);
+                } else {
+                    $aliasTable = $this->parseKey($table);
+                }
+                $item[] =  $database.'.'.$aliasTable;
+            }
+        }
+        return implode(',', $item);
+    }
+
+    /**
+     * 随机排序
+     * @access protected
+     * @param  Query $query 查询对象
+     * @return string
+     */
+    protected function parseRand()
+    {
+        return 'DBMS_RANDOM.value';
+    }
+
+}
---
+++ thinkphp/library/think/db/connector/Dm.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace think\db\connector;
+
+use PDO;
+use think\db\Connection;
+
+/**
+ * Dm数据库驱动
+ */
+class Dm extends Connection
+{
+    protected $builder = '\\think\\db\\builder\\Dm';
+
+    /**
+     * 解析pdo连接的dsn信息
+     * @access protected
+     * @param array $config 连接信息
+     * @return string
+     */
+    protected function parseDsn($config)
+    {
+        $dsn = 'dm:';
+        if (!empty($config['hostname'])) {
+            $dsn .=  $config['hostname'] . ($config['hostport'] ? ':' . $config['hostport'] : '') . '/';
+        }
+        if (!empty($config['charset'])) {
+            $dsn .= ';charset=' . $config['charset'];
+        }
+
+        return $dsn;
+    }
+
+    /**
+     * 取得数据表的字段信息
+     * @access public
+     * @param string $tableName
+     * @return array
+     */
+    public function getFields($tableName)
+    {
+        list($tableName) = explode(' ', $tableName);
+        $sql             = "select a.column_name,data_type,DECODE (nullable, 'Y', 0, 1) notnull,data_default, DECODE (A .column_name,b.column_name,1,0) pk from all_tab_columns a,(select column_name from all_constraints c, all_cons_columns col where c.constraint_name = col.constraint_name and c.constraint_type = 'P' and c.table_name = '" . strtoupper($tableName) . "' ) b where table_name = '" . strtoupper($tableName) . "' and a.column_name = b.column_name (+)";
+
+        $pdo    = $this->query($sql, [], false, true);
+        $result = $pdo->fetchAll(PDO::FETCH_ASSOC);
+        $info   = [];
+
+        if ($result) {
+            foreach ($result as $key => $val) {
+                $val                       = array_change_key_case($val);
+                $info[$val['column_name']] = [
+                    'name'    => $val['column_name'],
+                    'type'    => $val['data_type'],
+                    'notnull' => $val['notnull'],
+                    'default' => $val['data_default'],
+                    'primary' => $val['pk'],
+                    'autoinc' => $val['pk'],
+                ];
+            }
+        }
+
+        return $this->fieldCase($info);
+    }
+
+    /**
+     * 取得数据库的表信息（暂时实现取得用户表信息）
+     * @access   public
+     * @param string $dbName
+     * @return array
+     */
+    public function getTables($dbName = '')
+    {
+        $sql    = 'select table_name from all_tables';
+        $pdo    = $this->query($sql, [], false, true);
+        $result = $pdo->fetchAll(PDO::FETCH_ASSOC);
+        $info   = [];
+
+        foreach ($result as $key => $val) {
+            $info[$key] = current($val);
+        }
+
+        return $info;
+    }
+
+    /**
+     * SQL性能分析
+     * @access protected
+     * @param string $sql
+     * @return array
+     */
+    protected function getExplain($sql)
+    {
+        return [];
+    }
+
+    protected function supportSavepoint()
+    {
+        return true;
+    }
+}
